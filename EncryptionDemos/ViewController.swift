d9a30cdb67bfd710b79e3c8691ee19fe539b1031
Deepak P. Patil <dpatil@ios-dpatil.local>
2018-06-27 14:22:58 +0530

    Initial Commit

---
+++ EncryptionDemos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EncryptionDemos
+//
+//  Created by Deepak P. Patil on 27/06/18.
+//  Copyright © 2018 Deepak P. Patil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
